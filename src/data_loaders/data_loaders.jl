ccdecc7e14a783e4763cb6f864e023a267593ad9
abiprakash007@gmail.com <abiprakash007@gmail.com>
2021-05-13 14:26:37 +1000

    data loaders

--- src/data_loaders/data_loaders.jl
+++ src/data_loaders/data_loaders.jl
@@ -9,12 +9,21 @@ function list_data_by_years(dir::String; fformat::String="csv",
     return files
 end
 
+"""
+    load_parquet_data(files::Array{String}; str::String="data")
+
+Loads parquet files (path to files in 'files' variable)
+
+# Arguments
+- `str::String`: data description printed when loading data
+"""
 function load_parquet_data(files::Array{String}; str::String="data")
-    dfs = DataFrame[]
+    n = length(files)
+    dfs = Array{DataFrame}(undef, n)
     println("Loading $str")
-    @showprogress for file in files
-        chunk = DataFrame(read_parquet(file, use_threads=true))
-        push!(dfs, chunk)
+    @showprogress for i in 1:n
+        chunk = DataFrame(read_parquet(files[i], use_threads=true))
+        dfs[i] = chunk
     end
     df = vcat(dfs...)
     return df
