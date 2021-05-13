c46eafbb63e5083fd01631a2fad8146f00058767
abiprakash007@gmail.com <abiprakash007@gmail.com>
2021-05-13 14:26:50 +1000

    latest export file

--- src/FCASanova.jl
+++ src/FCASanova.jl
@@ -12,11 +12,13 @@ using Query
 include("aggregation/groupbys.jl")
 include("constraint_helpers/constraint_helpers.jl")
 include("data_loaders/data_loaders.jl")
+include("filters/filters.jl")
 include("mappers/mappers.jl")
 include("plot_helpers/plot_settings.jl")
 
 export find_binding_constraints,
        filter_by_prefix,
+       filter_data_by_year,
        groupby_and_sum,
        groupby_and_count,
        impute_constraint_service,
